acdc1254f6fc427753deb0c83ba910dfa466a622
Dev Karan <devkaransinghsisodia.7@gmail.com>
2022-06-30 17:41:20 +0530

    book slots disabled

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const fs = require("fs");
 const path = require("path");
 const jwt = require("jsonwebtoken");
 const http = require("http");
-const cron = require("node-cron");
+var cron = require("node-cron");
 const socketio = require("socket.io");
 const getData = require("./func");
 
 const app = express();
 const server = http.createServer(app);
+
 const influx = new Influx.InfluxDB("http://localhost:8086/manisha");
 
 const io = socketio(server, {
@@ -28,6 +29,7 @@ app.use(
     extended: false,
   })
 );
+
 app.use(express.static(publicDirectoryPath));
 
 var SOCKET_LIST = {};
@@ -41,16 +43,17 @@ io.on("connection", (socket) => {
 });
 
 app.post("/api/autopost", async (req, res) => {
-  cron.schedule("*/2 * * * * *", async () => {
+  var i = 0;
+  var task = cron.schedule("*/2 * * * * *", async () => {
     // influx query
     try {
       const datapoint = getData();
       await influx.writePoints(
         [
           {
-            measurement: "sensors3",
+            measurement: "ue_4",
             tags: {
-              sensor_id: "climax_1",
+              sensor_id: "sensor_1",
               user_location: "greenhouse",
             },
             fields: {
@@ -67,9 +70,9 @@ app.post("/api/autopost", async (req, res) => {
       await influx.writePoints(
         [
           {
-            measurement: "sensors3",
+            measurement: "ue_4",
             tags: {
-              sensor_id: "climax_2",
+              sensor_id: "sensor_2",
               user_location: "greenhouse",
             },
             fields: {
@@ -87,20 +90,24 @@ app.post("/api/autopost", async (req, res) => {
       let data1, data2;
       try {
         data1 = await influx.query(
-          `select * from sensors3 where sensor_id='climax_1'`
+          `select * from ue_4 where sensor_id='sensor_1'`
         );
         data2 = await influx.query(
-          `select * from sensors3 where sensor_id='climax_2'`
+          `select * from ue_4 where sensor_id='sensor_2'`
         );
       } catch (e) {
         return res.json({ message: e.message });
       }
 
       socket1.emit("data", data1, data2);
+      i++;
     } catch (e) {
       return res.json({ message: e.message });
     }
     console.log("done");
+    if (i == 61 * 60) {
+      task.stop();
+    }
   });
 
   res.status(201).json({ message: "success!" });
@@ -125,6 +132,75 @@ app.post("/api/login", (req, res) => {
   } else {
     res.json({ status: "error" });
   }
+});
+
+app.post("/api/getslots", async (req, res) => {
+  ue_name = req.body.ue_name;
+
+  try {
+    const t = await influx.query(
+      `select free_slots from ue_info where ue_name='${ue_name}'`
+    );
+
+    const t1 = JSON.parse(JSON.stringify(t))[0].free_slots;
+    var t2 = JSON.parse(t1);
+
+    res.json({ slots: t2 });
+  } catch (e) {
+    res.status(500).json({ message: e.message });
+  }
+});
+
+app.post("/api/reserve", async (req, res) => {
+  ue_name = req.body.ue_name;
+  schedule_date = req.body.date;
+  schedule_time = +req.body.time;
+  const t = await influx.query(
+    `select free_slots from ue_info where ue_name='${ue_name}'`
+  );
+
+  const t1 = JSON.parse(JSON.stringify(t))[0].free_slots;
+  var t2 = JSON.parse(t1);
+
+  const ind = t2.indexOf(schedule_time);
+
+  if (ind > -1) {
+    t2.splice(ind, 1);
+  }
+
+  var t3 = JSON.stringify(t2);
+
+  const u = await influx.query(
+    `select busy_slots from ue_info where ue_name='${ue_name}'`
+  );
+
+  const u1 = JSON.parse(JSON.stringify(u))[0].busy_slots;
+  var u2 = JSON.parse(u1);
+  u2.push(schedule_time);
+  var u3 = JSON.stringify(u2);
+  await influx.query(`delete from ue_info where ue='${ue_name}'`);
+
+  await influx.writePoints(
+    [
+      {
+        measurement: "ue_info",
+        tags: {
+          ue: ue_name,
+        },
+        fields: {
+          ue_name: ue_name,
+          is_busy: false,
+          free_slots: t3,
+          busy_slots: u3,
+        },
+      },
+    ],
+    {
+      database: "manisha",
+      precision: "ms",
+    }
+  );
+  res.json({ message: "slot booked" });
 });
 
 server.listen(1337, () => {
--- src/components/reservation/Slots.js
+++ src/components/reservation/Slots.js
@@ -5,31 +5,36 @@ import "./Slots.css";
 
 const Slots = (props) => {
   //we have the access of ue_name as ue_id;
+  const allSlots = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23,
+  ];
   const [slotList, setSlotList] = useState([]);
-  useEffect(async () => {
-    try {
-      const response = await fetch("http://localhost:1337/api/getslots", {
-        method: "post",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          ue_name: props.ue_name,
-        }),
-      });
-      if (!response.ok) {
-        throw new Error("Something went wrong");
+  useEffect(() => {
+    const func = async () => {
+      try {
+        const response = await fetch("http://localhost:1337/api/getslots", {
+          method: "post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ue_name: props.ue_name,
+          }),
+        });
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+        const data = await response.json();
+        setSlotList(data.slots);
+      } catch (e) {
+        console.log(e.message);
       }
-      const data = await response.json();
-      setSlotList(data.slots);
-    } catch (e) {
-      console.log(e.message);
-    }
-  }, [setSlotList]);
+    };
+    func();
+  }, [setSlotList, props.ue_name]);
   const bookSlotHandler = async (event) => {
-    console.log(event.target.value);
-    console.log(props.ue_name);
-    console.log(props.date);
+    // console.log(slotList);
     //send the http request
     try {
       const response = await fetch("http://localhost:1337/api/reserve", {
@@ -51,20 +56,28 @@ const Slots = (props) => {
     } catch (e) {
       console.log(e.message);
     }
+    setSlotList((prev) => {
+      return prev.filter((s) => s !== +event.target.value);
+    });
   };
   if (slotList.length === 0) {
-    return <h2>No slots available</h2>;
+    return <h2> No slots available </h2>;
   }
+  console.log(slotList);
+
   return (
     <Card className="slots">
-      <h2>Pick a time slot:</h2>
-      {slotList.map((slot) => (
+      <h2> Pick a time slot: </h2>
+      {allSlots.map((slot) => (
         <button
-          className="btn"
+          className={`btn ${
+            !slotList.find((s) => s === slot) && "btn-disabled"
+          }`}
           type="button"
           key={slot}
           onClick={bookSlotHandler}
           value={slot}
+          disabled={!slotList.find((s) => s === slot)}
         >
           {`${slot}:00 to ${(slot + 1) % 24}:00`}
         </button>
